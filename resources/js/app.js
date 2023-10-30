479996363841099466844f5fd01c474b5f434faa
Kamil <kamilczekotas@gmail.com>
2023-10-30 20:47:03 +0100

    feat: navbar done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import './bootstrap';
+import "@fortawesome/fontawesome-free/css/all.css";
+import $ from 'jquery';
+window.$ = $;
+
+$(".fa-brands").on("mouseenter", function () {
+    $(this).toggleClass('fa-shake');
+}
+).on("mouseleave", function () {
+    $(this).toggleClass('fa-shake');
+})
+
+$(window).on("scroll", function () {
+    if (window.scrollY >= 100) {
+        $('.navi').addClass('dark');
+    } else {
+        $('.navi').removeClass('dark');
+    }
+})
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Porachunki">
+
+    <title>Porachunki</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css2?family=Alegreya:wght@700&family=Raleway:wght@300&display=swap" rel="stylesheet">
+
+    @vite('resources/js/app.js')
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    @section('navbar')
+        <div class="container navi">
+            <div class="row">
+                <h4 class="col-md-6 col-sm-3 text-left">Porachunki</h4>
+                <div class="col-md-6 col-sm-9 text-right">
+                    <h4 class="px-3"><a href="" class="text">Home</a></h4>
+                    <h4 class="px-3"><a href="" class="text">Contact</a></h4>
+                    <h4><a href="https://www.instagram.com" target="_blank"><i class="fa-brands fa-instagram px-4"></i></a></h4>
+                    <h4><a href="https://www.facebook.com" target="_blank"><i class="fa-brands fa-facebook-f px-2"></i></a></h4>
+                </div>
+            </div>
+        </div>
+    @show
+
+        @yield('content')
+</body>
+
+</html>
