9e703096c33850cacd76a57ac0551cc67fcb6a67
Kamil <kamilczekotas@gmail.com>
2023-10-31 23:03:25 +0100

    feat: iframe with google maps

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,9 @@
 
 @section('content')
 <div class="block" id="home">
-
+    <div class="mapouter">
+        <div class="gmap_canvas"><iframe src="https://maps.google.com/maps?q=Mercedes,%20Towarowa,%20Olsztyn,%20Poland&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" style="width: 100%; height: 100%;"></iframe>
+        </div>
+    </div>
 </div>
 @stop
