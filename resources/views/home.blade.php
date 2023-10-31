91215053868827a9e6d30316fcea3ddd2aa5d070
Kamil <kamilczekotas@gmail.com>
2023-10-31 23:13:05 +0100

    feat: text under iframe

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,10 @@
     <div class="mapouter">
         <div class="gmap_canvas"><iframe src="https://maps.google.com/maps?q=Mercedes,%20Towarowa,%20Olsztyn,%20Poland&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=&amp;output=embed" frameborder="0" scrolling="no" style="width: 100%; height: 100%;"></iframe>
         </div>
+        <div class="map-text text-left py-2">
+            <p>Morozya, Kalerowicza 12</p>
+            <p>Olsztyn 80-300</p>
+        </div>
     </div>
 </div>
 @stop
